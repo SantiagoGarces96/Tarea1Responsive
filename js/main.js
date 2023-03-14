89a02bad6975cc597d137b7de0d56cb2d9886072
SantiagoGarces96 <santiagogarcesstory@gmail.com>
2023-03-14 02:49:24 -0500

    correciones

--- js/main.js
+++ js/main.js
@@ -4,16 +4,13 @@ const nav = document.querySelector(".nav-mobile");
 console.log(nav);
 
 function toggleDisplay(navBar) {
-    if (nav.style.display === 'none') {
-      nav.style.display = 'block';
-    } else {
-      nav.style.display = 'none';
-    }
+  if (nav.style.display === "none") {
+    nav.style.display = "block";
+  } else {
+    nav.style.display = "none";
   }
+}
 
-btn_collapse.addEventListener("click", ()=>{
-    
-    toggleDisplay(nav);
+btn_collapse.addEventListener("click", () => {
+  toggleDisplay(nav);
 });
-
-
